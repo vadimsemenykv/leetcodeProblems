a9d3952de846d5c82f677820633611804b3b9c24
Vadym Semeniuk <vadym.semeniuk@platech.com>
2021-06-28 17:45:14 +0300

    resolve RomanToInteger13

--- src/main/java/com/sv/leetcode/RomanToInteger13.java
+++ src/main/java/com/sv/leetcode/RomanToInteger13.java
@@ -6,7 +6,32 @@ package com.sv.leetcode;
  */
 
 public class RomanToInteger13 {
+    protected final static int[] charMap;
+
+    static {
+        charMap = new int[26];
+        charMap['I' - 'A'] = 1;
+        charMap['V' - 'A'] = 5;
+        charMap['X' - 'A'] = 10;
+        charMap['L' - 'A'] = 50;
+        charMap['C' - 'A'] = 100;
+        charMap['D' - 'A'] = 500;
+        charMap['M' - 'A'] = 1000;
+    }
+
     public int romanToInt(String s) {
-        return 0;
+        int res = 0;
+        int prev = 1000;
+        for (char cc : s.toCharArray()) {
+            int current = parse(cc);
+            if (current > prev) res -= 2 * prev;
+            res += current;
+            prev = current;
+        }
+        return res;
+    }
+
+    protected int parse(char ch) {
+        return charMap[ch - 'A'];
     }
 }
